f0576da191711f4e6ece3efa865e44bf2e066e2d
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-04-30 01:39:56 -0500

    message error placa

--- resources/js/Pages/Infraccion/Index.vue
+++ resources/js/Pages/Infraccion/Index.vue
@@ -50,8 +50,7 @@
               id="default-search"
               v-model="form.search"
               class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search Mockups, Logos..."
-              required
+              placeholder="Ingrese una placa"
             />
             <button
               type="submit"
@@ -93,6 +92,7 @@
             </tbody>
           </table>
         </div>
+        <div class="text-red-700" v-if="infraccion == 0"> No se encontrado ningun registro en la Base de datos</div>
       </div>
     </div>
   </AuthenticatedLayout>
