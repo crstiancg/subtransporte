4a61f9febb6679d4d99702e03f7d0d579a93ada6
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-10-10 20:08:58 -0500

    nueva interfacez de licencias

--- resources/js/Layouts/Frontend.vue
+++ resources/js/Layouts/Frontend.vue
@@ -1,15 +1,12 @@
 <script setup>
-import Header from "@/Components/Frontend/Header.vue";
-import Footer from "@/Components/Frontend/Footer.vue";
+
 </script>
 <template>
-  <div class="bg-white dark:bg-slate-900 ">
-    <!-- Header -->
-    <Header/>
-    <main class="h-screen ">
-      <slot/>
-    </main>
-    <Footer/>
-    <!-- Footer -->
+  <div class="min-h-screen flex flex-col sm:justify-normal items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <div class="max-w-[1600px] w-full rounded-full h-[396px] bg-gradient-to-b from-transparent from-0% via-90% to-100% via-gray-700 to-transparent absolute bg-opacity-50 left-1/2 -translate-x-1/2 top-[200px] blur-[100px]">
+    </div>
+    <div class="w-full sm:max-w-xl  px-6 lg:py-4 py-40 overflow-hidden sm:rounded-lg">
+            <slot />
+    </div>
   </div>
 </template>
--- resources/js/Pages/Licencia.vue
+++ resources/js/Pages/Licencia.vue
@@ -1,10 +1,10 @@
 <script setup>
-import Fronted from "@/Layouts/Frontend.vue";
-import { useForm, Link } from "@inertiajs/vue3";
+// import Fronted from "@/Layouts/Frontend.vue";
+import { useForm, Link, Head } from "@inertiajs/vue3";
 import { ref, onMounted, watch } from "vue";
 // import TextInput from "@/Components/TextInput.vue";
 // import { Head, , useForm } from '@inertiajs/vue3';
-
+import Fronted from "@/Layouts/Frontend.vue"
 defineProps({
   licencia: Object,
 });
@@ -32,194 +32,192 @@ const consultar = () => {
 };
 
 </script>
-      
 <template>
+  <Head title="GTV" />
   <Fronted>
-    <Section class="w-full mx-auto px-4 sm:px-6 lg:px-8 pb-24 py-20 h-[50vh]">
-      <!-- <div
-        class="bg-red-300 dark:bg-red-600 overflow-hidden shadow-sm sm:rounded-lg mt-4"
-      >
-        <div
-          class="text-center p-6 text-gray-900 font-extrabold dark:text-gray-100 text-4xl"
-        >
-          En fase de prueba
+      <div class="relative px-4 pb-0 pt-0 md:pt-[70px] -mt-[150px] md:mt-0">
+        <h1 class="max-w-[50rem] sm:max-w-[49rem] px-3 md:px-0 sm:text-5xl md:text-6xl text-4xl font-bold text-center text-gray-50 uppercase">Vehículo Autorizado
+        </h1>
+        <h2 class="max-w-[50rem] sm:max-w-[49rem]  z-10 font-semibold text-gray-400 mt-6 text-center px-4 text-lg">Gerencia de Transporte y Seguridad Vial </h2>
+      </div>
+      <form @submit.prevent="consultar" 
+      class="flex relative z-[21] mx-autom mt-8 max-w-[550px] hover:scale-105 w-full shadow-lg bg-gray-300 overflow-visible rounded-full transition-transform focus-visible:ring-sky-400 focus-visible:border-sky-400">
+      <input type="text" placeholder="Ingrese una placa" v-model="form.search" :maxlength="max" class="block w-full px-6 pl-10 py-4 pr-2 mr-px font-sans ouline-gray-200 font-normal  text-base border-none focus:ring-0 shadow-sm rounded-s-full bg-gray-300 text-dark-900 placeholder:text-gray-500">
+      <button type="submit" class="flex items-center px-5 py-1.5  pr-7 text-base gap-1.5 font-sans font-semibold transition-colors border-none cursor-pointer bg-gray-00 hover:bg-gray-400 text-dark-800 group  rounded-e-full rounded-t-full rounded-s-full pointer "><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-dark-800 transition-colors"><path d="M21 21L14.9497 14.9497M14.9497 14.9497C16.2165 13.683 17 11.933 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17C11.933 17 13.683 16.2165 14.9497 14.9497Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg><span>Buscar</span></button>
+    </form>
+    <div class="text-center" v-if="form.search.length>0 && form.search.length <= 5 ">
+      <span class="text-green-400 text-sm">Ingrese su placa en el formato: AKF952</span>
+    </div>
+
+    <div class="relative" v-if="licencia ? true : hidden">
+      <div class="bg-gray-800 dark:text-white max-w-md mx-auto rounded-xl shadow-xl overflow-hidden md:max-w-2xl m-5 hover:shadow-4xl hover:bg-gray-950 transition-all duration-500 ease-in-out">
+        <div class="justify-center ">
+          <div class="p-6" v-for="licencia in licencia"
+          :key="licencia.id">
+            <div class="text-center">
+              <span class="text-3xl font-extrabold text-green-400">{{ licencia.placa }} - {{licencia.codigo}}</span>
+            </div>
+            <div class="uppercase tracking-wide text-xl text-green-500 font-semibold text-center">Información del vehiculo</div>
+            <p class="block mt-1 text-lg leading-tight font-medium text-white uppercase">Propietario</p>
+            <p class="mt-2 text-gray-400">{{ licencia.nombre_conductor }}</p>
+            <p class="block mt-1 text-lg leading-tight font-medium text-white uppercase">Empresa</p>
+            <p class="mt-2 text-gray-400">{{ licencia.empresa }}</p>
+            <p class="block mt-1 text-lg leading-tight font-medium text-white uppercase">Dirección</p>
+            <p class="mt-2 text-gray-400">{{ licencia.ruta }}</p>
+            <div class="flex items-center justify-center p-2 space-x-4 lg:hidden">
+              <button style="margin-right: 20px" class="text-red-500 hover:text-white">
+                <div class="image">
+                  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 7L9.00004 18L3.99994 13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                  </div>
+              </button>
+            </div>
+          </div>
         </div>
-      </div> -->
-      <h1
-        class="font-extrabold dark:text-gray-100 text-4xl text-center mt-10 mb-6"
-      >
-        Vehículo Autorizado Gtsv
-      </h1>
+      </div>
+    </div>
 
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <!-- Buton Create -->
-        <div
-          class="bg-white shadow-[6px_0px_24px_0px_#cbd5e0] dark:shadow-sm rounded-lg py-2 dark:bg-gray-800"
-        >
-        <div class="lg:grid justify-items-center m-2">
-          <div class="flex items-center justify-end mt-4">
-            <Link
-            :href="route('licencias.index')"
-             class="items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" 
-             >Limpiar</Link>
-          </div>
-          <form @submit.prevent="consultar" class="py-4">
-              <div class="relative">
-                <div
-                  class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
-                >
-                  <svg
-                    aria-hidden="true"
-                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    ></path>
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  id="default-search"
-                  v-model="form.search"
-                  :maxlength="max"
-                  class="block w-full lg:w-80 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Ingrese una placa: AFD708"
-                />
-                <button
-                  type="submit"
-                  class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Buscar
-                </button>
-              </div>
-            <!-- <div v-if="form.search.length>=6">
-              <span class="text-red-300 text-sm">El campo puede tener hasta 6 caracteres</span>
-            </div> -->
-            <div v-if="form.search.length>0 && form.search.length <= 5 ">
-              <span class="text-green-300 text-sm">Ingrese su placa en el formato: AKF952</span>
-            </div>
-            </form>
-          </div>
-
-          <div
-            class="relative overflow-x-auto py-4"
-            v-if="licencia ? true : hidden"
-          >
-            <!-- probando -->
-            <div
-              id="alert-additional-content-3"
-              class="grid justify-items-center p-4 mb-4 mt-4 text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800 lg:m-full m-2"
-              role="alert"
-              v-for="licencia in licencia"
-              :key="licencia.id"
-            >
-              <div class="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-                <span class="sr-only">Info</span>
-                <h3 class="lg:text-3xl text-lg font-medium mx-2">VEHICULO AUTORIZADO</h3>
-              </div>
-              <div class="mt-2 mb-4 text-sm">
-                <div class="text-center">
-                  <h1 class="font-bold lg:text-5xl text-3xl">{{ licencia.placa }} - {{licencia.codigo}}</h1>
-                </div>
-                <div>
-                  <!-- <ul class="mt-1.5 ml-4 list-disc list-inside"> -->
-                  <p class="lg:text-xl font-bold  dark:text-gray-300 text-gray-800">PROPIETARIO: <span class="font-semibold dark:text-teal-600 text-teal-600 lg:text-xl  text-xs">{{ licencia.nombre_conductor }}</span></p>
-                  <p class="lg:text-xl font-bold  dark:text-gray-300 text-gray-800">EMPRESA: <span class="font-semibold dark:text-teal-600 text-teal-600 lg:text-xl  text-xs">{{ licencia.empresa }}</span></p>
-                  <p class="lg:text-xl font-bold  dark:text-gray-300 text-gray-800">DIRECCIÓN: <span class="font-semibold dark:text-teal-600 text-teal-600 lg:text-xl  text-xs">{{ licencia.ruta }}</span></p>
-                <!-- </ul> -->
-                </div>
-              </div>
-            </div>
-            <!-- fin -->
-          </div>
-          <div v-if="licencia == 0">
-            <div
-              id="alert-additional-content-2"
-              class="grid justify-items-center p-4 m-2 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
-              role="alert"
-            >
-              <div class="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
+    <div v-if="licencia == 0">
+      <div class="card">
+        <div class="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-gray-300">
+              <linearGradient id="gradientColor">
+                  <stop offset="5%" stop-color="#5a0202"></stop>
+                  <stop offset="95%" stop-color="#e81515"></stop>
+              </linearGradient>
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     d="M12 9v3.75m0-10.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.249-8.25-3.286zm0 13.036h.008v.008H12v-.008z"
                   />
-                </svg>
-
-                <span class="sr-only">Info</span>
-                <h3 class="font-bold  lg:text-3xl text-lg mx-4">
-                  VEHICULO NO AUTORIZADO
-                </h3>
-              </div>
-              <div class="text-center">
-                <span class="lg:text-4xl text-3xl">{{ form.search }}</span>
-                <!-- <h1 class="font-bold lg:text-4xl text-3xl">{{form.search}}</h1> -->
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          v-show="licencia ? true : false"
-          class="bg-gray-800 dark:bg-gray-800 shadow-[4px_1px_5px_1px_#a0aec0] dark:shadow-sm rounded-lg mt-4"
-        >
-          <div
-            class="p-2 text-center text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
-            role="alert"
-          >
-            <svg
-              aria-hidden="true"
-              class="flex-shrink-0 inline w-5 h-5 mr-3"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                clip-rule="evenodd"
-              ></path>
-              
             </svg>
-            <span class="sr-only">Info</span>
-            <div>
-              <span class="font-medium">Info Alerta!</span> Mayor información en la
-              Gerencia de Transportes.
-            </div>
-          </div>
         </div>
+        <p class="title">Vehiculo no Autorizado</p>
+        <div class="text-center">
+          <span class="text-3xl font-extrabold text-red-400 uppercase">{{ form.search }}</span>
+        </div>
+        <p class="text">Mayor información en la Gerencia de Transportes.</p>
       </div>
-    </Section>
+    </div>
+    
+  <!-- <div class="error">
+    <div class="error__icon">
+        <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m13 13h-2v-6h2zm0 4h-2v-2h2zm-1-15c-1.3132 0-2.61358.25866-3.82683.7612-1.21326.50255-2.31565 1.23915-3.24424 2.16773-1.87536 1.87537-2.92893 4.41891-2.92893 7.07107 0 2.6522 1.05357 5.1957 2.92893 7.0711.92859.9286 2.03098 1.6651 3.24424 2.1677 1.21325.5025 2.51363.7612 3.82683.7612 2.6522 0 5.1957-1.0536 7.0711-2.9289 1.8753-1.8754 2.9289-4.4189 2.9289-7.0711 0-1.3132-.2587-2.61358-.7612-3.82683-.5026-1.21326-1.2391-2.31565-2.1677-3.24424-.9286-.92858-2.031-1.66518-3.2443-2.16773-1.2132-.50254-2.5136-.7612-3.8268-.7612z" fill="#393a37"></path></svg>
+    </div>
+    <div class="error__title">Info Alerta! Mayor información en la Gerencia de Transportes.</div>
+  </div> -->
   </Fronted>
 </template>
-<script type="text/javascript">
-document.oncontextmenu = function(){return false;}
-</script>
+
+<style scoped>
+.image {
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #00be9e;
+  flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 9999px;
+  animation: animate .6s linear alternate-reverse infinite;
+  transition: .6s ease;
+}
+
+.image svg {
+  color: #00e3d0;
+  width: 1rem;
+  height: 1rem;
+}
+
+@keyframes animate {
+  from {
+    transform: scale(0.9);
+  }
+
+  to {
+    transform: scale(1.3);
+  }
+}
+.card {
+  height: 270px;
+  background-color: #1E293B;
+  margin: 10px;
+  border-radius: 8px;
+  box-shadow: 0px 2px 10px #1E293B;
+  border: 2px solid #1E293B;
+  font-size: 16px;
+  transition: all 0.3s ease;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: 'Poppins', sans-serif;
+}
+
+.icon {
+  margin: 0 auto;
+  width: 100%;
+  height: 80px;
+  max-width: 80px;
+  background: linear-gradient(90deg, #5a0202 0%, #e81515 40%, rgba(0, 0, 0, 0.28) 60%);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.8s ease;
+  background-position: 0px;
+  background-size: 200px;
+}
+
+.icon svg {
+  fill: #3f010100;
+}
+
+.card .title {
+  width: 100%;
+  margin: 0;
+  text-align: center;
+  margin-top: 30px;
+  color: white;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+}
+
+.card .text {
+  width: 80%;
+  margin: 0 auto;
+  font-size: 13px;
+  text-align: center;
+  margin-top: 20px;
+  color: white;
+  font-weight: 200;
+  letter-spacing: 2px;
+  opacity: 0;
+  max-height: 0;
+  transition: all 0.3s ease;
+}
+
+.card:hover {
+  height: 270px;
+  background-color: rgb(3 7 18);
+}
+
+.card:hover .text {
+  transition: all 0.3s ease;
+  opacity: 1;
+  max-height: 40px;
+}
+
+.card:hover .icon {
+  background-position: -120px;
+  transition: all 0.3s ease;
+}
+
+.card:hover .icon svg path {
+  fill: url('#gradientColor');
+  transition: all 0.1s ease;
+}
+</style>
